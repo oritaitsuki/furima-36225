cad3232a23af49db894e401a698b4d1b617ca80e
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-07-30 14:51:10 +0900

    新規登録画面実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:read_first])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:read_family])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth])
+  end
 end
