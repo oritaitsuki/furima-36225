aef699fd10f4a14ad95b758656174112f0f5d7c8
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-07-30 17:11:25 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :read_first, presence: true
+  validates :read_family, presence: true
+  validates :birth, presence: true
 end
