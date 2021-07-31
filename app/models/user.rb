44869adc4b6ecbea24c2429aa473355e4ae1cf5c
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-07-31 19:37:10 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-          validates :first_name
-          validates :family_name
+    validates :first_name
+    validates :family_name
   end
-  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'} do
-          validates :read_first
-           validates :read_family
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' } do
+    validates :read_first
+    validates :read_family
   end
-  validates :encrypted_password,     presence: true, length: { minimum: 6 } 
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
   validates :nickname, presence: true
   validates :birth, presence: true
 end
