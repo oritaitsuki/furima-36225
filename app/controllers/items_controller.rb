52e0e46520dd5e1f9584870bd95b67ff6a3eb908
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-01 22:16:06 +0900

    ストロングパラメータ設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+ 
   def index
   end
 
@@ -7,6 +8,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+  end
+  
+  private
+  def item_params
+  params.require(:item).permit(:image, :items_name, :explanation, :category_id, :condition_id, :postage_type_id, 
+    :prefectures_id, :preparation_day_id, :value, :user).merge(user_id: current_user.id)
   end
 end
 
