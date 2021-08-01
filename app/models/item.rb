7cb57f0345cdf2c01be2c01045da7fe4eaf88f5c
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-01 21:58:41 +0900

    acctive Storage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  has_one :user_item
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :prefectures
+  belongs_to_active_hash :value
 end
