eee0143021da386ac510e2815611df304bd1e738
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-02 12:49:27 +0900

    acctive hash実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path 
+    else
+      render :new 
+    end
   end
+
   
   private
   def item_params
-  params.require(:item).permit(:image, :items_name, :explanation, :category_id, :condition_id, :postage_type_id, 
+  params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :postage_type_id, 
     :prefectures_id, :preparation_day_id, :value, :user).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,33 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :category_id, presence: true
+  
+  belongs_to :condition
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, presence: true
+  
+  belongs_to :postage_type
+  validates :postage_type_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :postage_type_id, presence: true
+
+  belongs_to :prefectures
+  validates :prefectures_id, numericality: { other_than: 0 , message: "can't be blank"} 
+  validates :prefectures_id, presence: true
+
+  belongs_to :preparation_day
+  validates :preparation_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :preparation_day_id, presence: true
+
+
+ 
+  
   has_one_attached :image
   belongs_to :user
   has_one :user_item
-  belongs_to_active_hash :category
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :postage_type
-  belongs_to_active_hash :prefectures
-  belongs_to_active_hash :value
+  
+  
+  
+  
 end
