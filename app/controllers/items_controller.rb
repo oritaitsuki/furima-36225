c0c9b8d2921f3173d76d947c57d5be82ad1afd0b
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-03 20:46:43 +0900

    編集画面追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -21,10 +21,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item= Item.find(params[:id])
+    move_to_index
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :postage_type_id,
                                  :prefectures_id, :preparation_day_id, :value).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless @item.user_id == current_user.id
+     redirect_to action: :index
+    end
   end
 end
