b6340be1a4a7135b25f4d165a08b0d84302c94ea
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-03 23:04:17 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,15 @@ class ItemsController < ApplicationController
     move_to_index
   end
 
+  def update
+    @item= Item.find(params[:id])
+    if @item.update (item_params)
+      redirect_to  items_path
+    else
+      render :edit 
+    end
+  end
+
   private
 
   def item_params
