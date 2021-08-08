a7625dbe78cfb783c4a485fdda49554cde266937
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-08 19:29:30 +0900

    修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,16 +1,4 @@
 class Address < ApplicationRecord
-  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
-    validates :prefectures_id
-  end
-
-  with_options presence: true do
-    validates :post_number
-    validates :city
-    validates :address
-    validates :phone_number
-  end
-  attr_accessor :token
-
+ 
   belongs_to :user_item
-  validates :token, presence: true
 end
