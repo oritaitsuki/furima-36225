279283ce37c9bd799ecbca3847c699378dc952a4
oritaitsuki <87003753+oritaitsuki@users.noreply.github.com>
2021-08-18 17:45:17 +0900

    プレビュー表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../card")
 require("../item_value");
+require('../preview')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
